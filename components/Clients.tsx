254c986892007cfd0c32df18bca22b37f8788b8a
Saharsh <124720803+saharsh3008@users.noreply.github.com>
2024-07-08 23:06:45 +0530

    Update Clients.tsx

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -19,8 +19,8 @@ const Clients = () => {
         <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg: mt-10'>
             {companies.map(({id, img, name, nameImg }) => (
                <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>  
-               <Image src={img} alt={name} className='md:w-10 w-5'/>
-               <Image src={nameImg} alt={name} className='md:w-24 w-20'/>
+               <Image src={Image} alt={name} className='md:w-10 w-5'/>
+               <Image src={nameImage} alt={name} className='md:w-24 w-20'/>
                </div>
             ))}
         </div>
