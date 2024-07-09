86fd39b076a4a6e30d933b2c2b246a6cc0a83a59
Saharsh <124720803+saharsh3008@users.noreply.github.com>
2024-07-09 11:46:29 +0530

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -8,8 +8,8 @@ const RecentProjects = () => {
   return (
     <div className='py-20' id='projects'>
         <h1 className='heading'>
-            a small section of {''}
-            <span className='text-purple'>recent projects</span>
+           My {''}
+            <span className='text-purple'>projects</span>
         </h1>
         <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
             {projects.map(({ id, title, des, img, iconLists, link}) => (
